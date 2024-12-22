23a3f3beced45b72c32d631880ccf90d9aeeb0a6
Turas <tomasz.czaplinski@cyfroweszkoly.pl>
2024-12-22 18:53:27 +0100

    Dodanie klasy planszy Utworzono klasę Board odpowiedzialną za generowanie nagród na planszy i ich przechowywanie Dodano metody GenerateRewards, CheckReward oraz DisplayBoard

--- BoardGameSimulator/Program.cs
+++ BoardGameSimulator/Program.cs
@@ -28,6 +28,43 @@ class Player : IPlayer
         Console.WriteLine($"{Name} zyskuje {points} punktów. Obecny wynik: {Score}");
     }
 }
+
+class Board
+{
+    public int Size { get; private set; }
+    private Dictionary<int, int> rewards = new Dictionary<int, int>();
+
+    public Board(int size)
+    {
+        Size = size;
+        GenerateRewards();
+    }
+
+    private void GenerateRewards()
+    {
+        Random random = new Random();
+        for (int i = 1; i <= Size / 2; i++)
+        {
+            int position = random.Next(1, Size + 1);
+            int points = random.Next(1, 11);
+            rewards[position] = points;
+        }
+    }
+
+    public int CheckReward(int position)
+    {
+        return rewards.ContainsKey(position) ? rewards[position] : 0;
+    }
+
+    public void DisplayBoard()
+    {
+        Console.WriteLine("Nagrody na planszy:");
+        foreach (var reward in rewards)
+        {
+            Console.WriteLine($"Pozycja {reward.Key}: {reward.Value} punktów");
+        }
+    }
+}
 internal class Program
 {
     public static void Main(string[] args)
