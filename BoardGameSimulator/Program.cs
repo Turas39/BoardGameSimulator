b5b765f7c67172bcd62d7e65672b5211fb4a87a3
Turas <tomasz.czaplinski@cyfroweszkoly.pl>
2024-12-22 18:47:26 +0100

    Inicjalizacja Projektu z interfejsem gracza Dodano interfejs IPlayer z właściwościami Name, Position, Score oraz metodami Move i UpdateScore

--- BoardGameSimulator/Program.cs
+++ BoardGameSimulator/Program.cs
@@ -1,6 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 
+interface IPlayer
+{
+    string Name { get; set; }
+    int Position { get; set; }
+    int Score { get; set; }
+    void Move(int steps);
+    void UpdateScore(int points);
+}
 internal class Program
 {
     public static void Main(string[] args)
