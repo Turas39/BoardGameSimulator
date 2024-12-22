560ea6baa531bf63889514efb3231c8780d9ef9e
Turas <tomasz.czaplinski@cyfroweszkoly.pl>
2024-12-22 18:50:48 +0100

    Dodanie klasy gracza Utworzono klasę Player implementującą interfejs IPlayer Dodano metody Move oraz UpdateScore z wyświetlaniem komunikatów

--- BoardGameSimulator/Program.cs
+++ BoardGameSimulator/Program.cs
@@ -9,6 +9,25 @@ interface IPlayer
     void Move(int steps);
     void UpdateScore(int points);
 }
+
+class Player : IPlayer
+{
+    public string Name { get; set; }
+    public int Position { get; set; } = 0;
+    public int Score { get; set; } = 0;
+
+    public void Move(int steps)
+    {
+        Position += steps;
+        Console.WriteLine($"{Name} przemieszcza się na pozycję {Position}.");
+    }
+
+    public void UpdateScore(int points)
+    {
+        Score += points;
+        Console.WriteLine($"{Name} zyskuje {points} punktów. Obecny wynik: {Score}");
+    }
+}
 internal class Program
 {
     public static void Main(string[] args)
