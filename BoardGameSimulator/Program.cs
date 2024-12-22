dce8477f9e5c4d2a8eb7e993f399d30e43daeb3d
Turas <tomasz.czaplinski@cyfroweszkoly.pl>
2024-12-22 18:44:09 +0100

    Dodane klase Program z metodą statyczną Main

--- BoardGameSimulator/Program.cs
+++ BoardGameSimulator/Program.cs
@@ -1,3 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;
 
-Console.WriteLine("Hello, World!");
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
